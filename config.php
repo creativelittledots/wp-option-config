3739b81090884c6134da3bbe34169a9200c846b4
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-07-11 11:38:00 +0100

    Initial Commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	
+/** Define ABSPATH as this file's directory */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+	
+// config.php
+
+return [
+
+	'test_option' => 'Test text'
+
+];
---
+++ mu-plugins/wp-option-config.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: WP Option Config
+Description: Checks config.php for options when calling get_option function
+Author: Creative Little Dots
+Version: 1.0
+Author URI: https://creativelittledots.co.uk
+License: GPL2
+*/
+
+namespace OptionConfig;
+
+class Config {
+	
+	public static $config = [];
+	
+	public static function init() {
+		
+		return new static();
+		
+	}
+	
+	public function __construct() {
+		
+		if( ! defined( 'CONFIG_PATH' ) ) {
+
+			define( 'CONFIG_PATH', ABSPATH . 'config.php' );
+			
+		}
+		
+		if( file_exists( CONFIG_PATH ) ) {
+			
+			static::$config = include CONFIG_PATH;
+			
+			add_filter( 'all', function($value) { 
+				
+				if( count( func_get_args() ) == 2 ) {
+				
+					list($value, $key) = func_get_args();
+					
+					if ( is_string( $key ) && strpos( $key, 'pre_option_' ) !== false ) {
+			
+						return static::get($key, $value); 
+						
+					}
+					
+				}
+				
+				return $value;
+			
+			}, 10, 2 );
+			
+		}
+		
+	}
+	
+	public static function get($key, $default = false) {
+		
+		return is_array( static::$config ) && isset( static::$config[$key] ) ? static::$config[$key] : $default;
+		
+	}
+	
+}
+
+Config::init();
+
+function config($key, $default = false) {
+
+	return Config::get($key, $default);
+
+}
+
+//echo config( 'test_option' );
